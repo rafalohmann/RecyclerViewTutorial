e65654fda1e8ffc285154e87e727276e01628aa1
Rafa Lohmann <raflohmann@gmail.com>
2018-12-26 12:54:08 -0200

    Part 2 - Adapter, LayoutManager, RecyclerView

---
+++ app/src/main/java/com/example/tutorial/recyclerviewproject/ExampleAdapter.java
@@ -0,0 +1,57 @@
+package com.example.tutorial.recyclerviewproject;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder> {
+    private ArrayList<ExampleItem> mExampleList;
+
+    public static class ExampleViewHolder extends RecyclerView.ViewHolder {
+        private ImageView mImageView;
+        private TextView mTextView1;
+        private TextView mTextView2;
+
+        public ExampleViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.imageView);
+            mTextView1 = itemView.findViewById(R.id.textView);
+            mTextView2 = itemView.findViewById(R.id.textView2);
+        }
+    }
+
+    public ExampleAdapter(ArrayList<ExampleItem> exampleList) {
+        mExampleList = exampleList;
+    }
+
+    @NonNull
+    @Override
+    public ExampleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.example_item, parent, false);
+        ExampleViewHolder evh = new ExampleViewHolder(v);
+        return evh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExampleViewHolder holder, int position) {
+        if (mExampleList != null) {
+            ExampleItem exampleItem = mExampleList.get(position);
+            holder.mImageView.setImageResource(exampleItem.getImageResource());
+            holder.mTextView1.setText(exampleItem.getText1());
+            holder.mTextView2.setText(exampleItem.getText2());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mExampleList != null)
+            return mExampleList.size();
+        else return 0;
+    }
+}
--- app/src/main/java/com/example/tutorial/recyclerviewproject/MainActivity.java
+++ app/src/main/java/com/example/tutorial/recyclerviewproject/MainActivity.java
@@ -2,19 +2,34 @@ package com.example.tutorial.recyclerviewproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         ArrayList<ExampleItem> exampleList = new ArrayList();
-        exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 1","Line 1.1"));
-        exampleList.add(new ExampleItem(R.drawable.ic_audio, "Line 2","Line 2.1"));
-        exampleList.add(new ExampleItem(R.drawable.ic_sun, "Line 3","Line 3.1"));
+
+        setContentView(R.layout.activity_main);        for (int i = 1; i <= 10; i++) {
+            exampleList.add(new ExampleItem(R.drawable.ic_android, "Line " + i,"Line " + i + ".1"));
+            exampleList.add(new ExampleItem(R.drawable.ic_audio, "Line " + i,"Line " + i + ".1"));
+            exampleList.add(new ExampleItem(R.drawable.ic_sun, "Line " + i,"Line " + i + ".1"));
+        }
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new ExampleAdapter(exampleList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
